bdaa538f5f9b8d2e5efd8c8c397c8022428937cd
jennachoi <juncr1719@gmail.com>
2019-05-31 22:49:07 -0700

    Basic Two Pointers [Easy]

--- Coding-Interview-Patterns/02 Two Pointers/PairWithTargetSum.java
+++ Coding-Interview-Patterns/02 Two Pointers/PairWithTargetSum.java
@@ -7,6 +7,10 @@ Given an array of sorted numbers and a target sum,
 find a pair in the array whose sum is equal to the given target
 Write a function to return the indices of the two numbers (i.e. the pair)
 such that they add up to the given target.
+
+Input: [1, 2, 3, 4, 6], target=6
+Output: [1, 3]
+Explanation: The number at index 1 and 3 add up to 6: 2+4=6
 */
 
 import java.util.HashMap;
@@ -42,7 +46,7 @@ class PairWithTargetSum {
     return new int[] {-1, -1};
   }/*
   Time Complexity: O(N)
-  Space Complexity: O(N)
+  Space Complexity: O(N); pushing N numbers in the HashTable.
   */
 
   public static void main(String[] args) {
