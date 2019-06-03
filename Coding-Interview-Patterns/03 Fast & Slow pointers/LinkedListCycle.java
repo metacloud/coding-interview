600b7bc4f95f52d0dd297ef8c0ff652978a0cef6
jennachoi <juncr1719@gmail.com>
2019-06-02 17:24:13 -0700

    LeetCode 141 [Easy]

--- Coding-Interview-Patterns/03 Fast & Slow pointers/LinkedListCycle.java
+++ Coding-Interview-Patterns/03 Fast & Slow pointers/LinkedListCycle.java
@@ -5,6 +5,11 @@ Pattern: Fast & Slow pointers
 
 Given the head of a Singly LinkedList,
 write a function to determine if the LinkedList has a cycle in it or not.
+
+# LeetCode 141. Linked List Cycle [Easy]
+[Result]
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Linked List Cycle.
+Memory Usage: 36 MB, less than 99.94% of Java online submissions for Linked List Cycle.
 */
 
 class ListNode {
