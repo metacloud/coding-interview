0fe58ce8953787c433ca8cf6cefa2e0360340555
jennachoi <juncr1719@gmail.com>
2019-06-09 00:23:01 -0700

    LeetCode 704 [Easy]

--- Coding-Interview-Patterns/11 Modified Binary Search/BinarySearch.java
+++ Coding-Interview-Patterns/11 Modified Binary Search/BinarySearch.java
@@ -16,27 +16,27 @@ Memory Usage: 39.4 MB, less than 94.49% of Java online submissions for Binary Se
 
 class BinarySearch {
 	public static int search(int[] arr, int key){
-			//Index value
-			int start = 0;
-			int end = arr.length-1;
-			int mid = start + (end-start)/2;
+		//Index value
+		int start = 0;
+		int end = arr.length-1;
+		int mid = start + (end-start)/2;
 
-			if(arr[start] < arr[end]){ // ascending order
-					while(start <= end){
-						mid = start + (end-start)/2;
-						if(arr[mid] == key) return mid;
-						else if(key < arr[mid]) end = mid-1;
-						else start = mid+1;
-					}
+		if(arr[start] < arr[end]){ // ascending order
+			while(start <= end){
+				mid = start + (end-start)/2;
+				if(arr[mid] == key) return mid;
+				else if(key < arr[mid]) end = mid-1;
+				else start = mid+1;
 			}
+		}
 
-			else { // descending order
-					while(start <= end){
-						mid = start + (end-start)/2;
-						if(arr[mid] == key) return mid;
-						else if(key < arr[mid]) start = mid+1;
-						else end = mid-1;
-					}
+		else { // descending order
+				while(start <= end){
+					mid = start + (end-start)/2;
+					if(arr[mid] == key) return mid;
+					else if(key < arr[mid]) start = mid+1;
+					else end = mid-1;
+				}
 			}
 			return -1;
 	}/*
@@ -45,10 +45,10 @@ class BinarySearch {
 	*/
 
 	public static void main(String[] args){
+
 		System.out.println(BinarySearch.search(new int[] { 4, 6, 10 }, 10));
     System.out.println(BinarySearch.search(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 5));
     System.out.println(BinarySearch.search(new int[] { 10, 6, 4 }, 10));
     System.out.println(BinarySearch.search(new int[] { 10, 6, 4 }, 4));
-
 	}
 }
