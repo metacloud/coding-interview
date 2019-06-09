feee6c04db5481e0c70955e55566ae4158dbe777
jennachoi <juncr1719@gmail.com>
2019-06-09 13:33:42 -0700

    GeeksforGeeks [Medium]

---
+++ Coding-Interview-Patterns/11 Modified Binary Search/CeilingOfANumber.java
@@ -0,0 +1,43 @@
+/*
+Pattern: Modified Binary Search
+02 Ceiling of a Number (medium)
+
+Given an array of numbers sorted in an ascending order,
+find the ceiling of a given number ‘key’.
+The ceiling of the ‘key’ will be the smallest element in the given array greater than or equal to the ‘key’.
+
+Write a function to return the index of the ceiling of the ‘key’. If there isn’t any ceiling return -1.
+*/
+
+class CeilingOfANumber {
+
+  public static int searchCeilingOfANumber(int[] arr, int key){
+    //using binary search property
+    int start = 0, end = arr.length-1;
+    int ceilingVal = -1;
+
+    //this is sorted order in ascending
+    while(start<=end){
+      int mid = start + (end-start)/2;
+      if(arr[mid] == key) return mid;
+      else if(arr[mid] < key){
+        start = mid+1;
+      }
+      else { //arr[mid]>key
+        ceilingVal = mid;
+        end = mid-1;
+      }
+    }
+    return ceilingVal;
+  }/*
+  Time Complexity: O(logN)
+  Space Complexity: O(1)
+  */
+
+  public static void main(String[] args) {
+    System.out.println(CeilingOfANumber.searchCeilingOfANumber(new int[] { 4, 6, 10 }, 6));
+    System.out.println(CeilingOfANumber.searchCeilingOfANumber(new int[] { 1, 3, 8, 10, 15 }, 12));
+    System.out.println(CeilingOfANumber.searchCeilingOfANumber(new int[] { 4, 6, 10 }, 17));
+    System.out.println(CeilingOfANumber.searchCeilingOfANumber(new int[] { 4, 6, 10 }, -1));
+  }
+}
