b53939a76892d22ba22ff438cc8303f2a30f3bb6
jennachoi <juncr1719@gmail.com>
2019-06-08 20:17:06 -0700

    LeetCode 102 [Medium]

--- Coding-Interview-Patterns/07 Tree BFS/LevelOrderTraversal.java
+++ Coding-Interview-Patterns/07 Tree BFS/LevelOrderTraversal.java
@@ -68,3 +68,24 @@ class LevelOrderTraversal {
     System.out.println("Level order traversal: " + result);
   }
 }
+
+
+// Using Recursion
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root){
+        List<List<Integer>> result = new ArrayList<>();
+        if(root==null) return result;
+        recursiveTraversal(root, 0, result);
+        return result;
+    }
+
+    public void recursiveTraversal(TreeNode root, int level, List<List<Integer>> result){
+        if(root==null) return;
+        if(result.size() <= level){
+            result.add(new ArrayList<Integer>());
+        }
+        result.get(level).add(root.val);
+        recursiveTraversal(root.left, level+1, result);
+        recursiveTraversal(root.right, level+1, result);
+    }
+}
