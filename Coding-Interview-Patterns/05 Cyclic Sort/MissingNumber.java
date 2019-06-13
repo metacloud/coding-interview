16415ce3f7dab6d076242fad41cbfa556a6cc0e3
jennachoi <juncr1719@gmail.com>
2019-06-12 17:58:41 -0700

    LeetCode 268 [Easy]

--- Coding-Interview-Patterns/05 Cyclic Sort/MissingNumber.java
+++ Coding-Interview-Patterns/05 Cyclic Sort/MissingNumber.java
@@ -6,13 +6,15 @@ Pattern: Cyclic Sort
 We are given an array containing ‘n’ distinct numbers taken from the range 0 to ‘n’.
 Since the array has only ‘n’ numbers out of the total ‘n+1’ numbers, find the missing number.
 
+Input: [8, 3, 5, 2, 4, 6, 0, 1]
+Output: 7
+
 # LeetCode 268. Missing Number [Easy]
 [Result]
 SUM >= XOR >= Cyclic Sort
 */
 
 class MissingNumber {
-
 
   public static int findMissingNumber(int[] nums) {
       int i=0;
