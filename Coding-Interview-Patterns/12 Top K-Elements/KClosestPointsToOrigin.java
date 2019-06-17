91b80583986363b347c3eb286b22f99fbf19d28d
jennachoi <juncr1719@gmail.com>
2019-06-17 15:20:31 -0700

    LeetCode 973 [Medium]

---
+++ Coding-Interview-Patterns/12 Top K-Elements/KClosestPointsToOrigin.java
@@ -0,0 +1,61 @@
+/*
+Pattern: Top 'K' Elements
+03 'K' Closest Points to the Origin (easy)
+
+Given an array of points in the a 2D2D plane, find ‘K’ closest points to the origin.
+
+Input: points = [[1,2],[1,3]], K = 1
+Output: [[1,2]]
+Explanation: The Euclidean distance between (1, 2) and the origin is sqrt(5).
+The Euclidean distance between (1, 3) and the origin is sqrt(10).
+Since sqrt(5) < sqrt(10), therefore (1, 2) is closer to the origin.
+*/
+
+
+import java.util.*;
+
+class Point {
+  int x;
+  int y;
+
+  public Point(int x, int y){
+    this.x = x;
+    this.y = y;
+  }
+
+  public int distFromOrigin(){
+    return (x*x)+(y*y);
+  }
+};
+
+class KClosestPointsToOrigin {
+  public static List<Point> findClosestPoints(Point[] points, int k){
+
+    //define maxHeap
+    PriorityQueue<Point> maxHeap = new PriorityQueue<>((p1, p2) -> Integer.compare(p2.distFromOrigin(), p1.distFromOrigin()));
+
+    for(int i=0; i<k; i++){
+      maxHeap.add(points[i]);
+    }
+
+    for(int i=k; i<points.length; i++){
+      if(points[i].distFromOrigin() < maxHeap.peek().distFromOrigin()){
+        maxHeap.poll();
+        maxHeap.add(points[i]);
+      }
+    }
+    return new ArrayList<>(maxHeap);
+  }/*
+  Time Complexity: k + (n-k) * log(k)
+  Space Complexity: O(k);
+  */
+
+  public static void main(String[] args){
+    Point[] points = new Point[] { new Point(1, 3), new Point(3, 4), new Point(2, -1) };
+    List<Point> result = KClosestPointsToOrigin.findClosestPoints(points, 2);
+    System.out.print("Here are the k points closest the origin: ");
+    for(Point p : result){
+      System.out.print("[" + p.x + " , " + p.y + "] ");
+    }
+  }
+}
