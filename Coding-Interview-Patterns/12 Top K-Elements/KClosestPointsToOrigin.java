cd704815c71674a68688d141fdcf2881d529658d
jennachoi <juncr1719@gmail.com>
2019-06-17 16:14:52 -0700

    LeetCode 973 [Medium]

--- Coding-Interview-Patterns/12 Top K-Elements/KClosestPointsToOrigin.java
+++ Coding-Interview-Patterns/12 Top K-Elements/KClosestPointsToOrigin.java
@@ -9,8 +9,12 @@ Output: [[1,2]]
 Explanation: The Euclidean distance between (1, 2) and the origin is sqrt(5).
 The Euclidean distance between (1, 3) and the origin is sqrt(10).
 Since sqrt(5) < sqrt(10), therefore (1, 2) is closer to the origin.
-*/
 
+# LeetCode 973. K Closest Points to Origin [Medium]
+[Result]
+Runtime: 52 ms, faster than 41.25% of Java online submissions for K Closest Points to Origin.
+Memory Usage: 64.5 MB, less than 15.93% of Java online submissions for K Closest Points to Origin.
+*/
 
 import java.util.*;
 
@@ -58,4 +62,28 @@ class KClosestPointsToOrigin {
       System.out.print("[" + p.x + " , " + p.y + "] ");
     }
   }
+}
+
+
+//LeetCode : Input type "2D Arrays"
+class Solution {
+    public int[][] kClosest(int[][] points, int K){
+
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+        (a,b)->Integer.compare(b[0]*b[0]+b[1]*b[1], a[0]*a[0]+a[1]*a[1]));
+
+        for(int i=0; i<K; i++){
+            maxHeap.add(points[i]);
+        }
+
+        for(int i=K; i<points.length; i++){
+            int[] max = maxHeap.peek();
+            if(max[0]*max[0]+max[1]*max[1] > points[i][0]*points[i][0] + points[i][1]*points[i][1]){
+                   maxHeap.poll();
+                   maxHeap.add(points[i]);
+               }
+        }
+        int[][] KClosests = new int[maxHeap.size()][];
+        return maxHeap.toArray(KClosests);
+      }
 }
