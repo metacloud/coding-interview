20e8cdea38dd7ed98c9fbe45adf8e4b2edc43a5c
jennachoi <juncr1719@gmail.com>
2019-06-28 16:20:37 -0700

    LeetCode 543 [Easy]

---
+++ Coding-Interview-Patterns/08 Tree DFS/TreeDiameter.java
@@ -0,0 +1,87 @@
+
+/*
+Pattern: Tree DFS
+06 Count Paths for a Sum (medium)
+
+Given a binary tree, find the length of its diameter.
+The diameter of a tree is the number of nodes on the longest path between any two leaf nodes.
+The diameter of a tree may or may not pass through the root.
+
+    1
+  /  \
+ 2    3
+ |   / \
+ 4  5  6
+
+Output: 5
+The diameter of the tree : [4, 2, 1, 3, 6]
+
+# LeetCode 543. Diameter of Binary Tree [Easy]
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Diameter of Binary Tree.
+Memory Usage: 39.3 MB, less than 23.11% of Java online submissions for Diameter of Binary Tree.
+*/
+
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int x) {
+    val = x;
+  }
+};
+
+class TreeDiameter {
+
+  static int maxDia = 0;
+  public static int findDiameter(TreeNode root) {
+
+    maxDepth(root);
+    return maxDia;
+  }
+
+  public static int maxDepth(TreeNode root){
+
+    if(root==null) return 0;
+    int left = maxDepth(root.left);
+    int right = maxDepth(root.right);
+    maxDia = Math.max(maxDia, left + right + 1);
+    return 1 + Math.max(left, right);
+  }
+
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    root.right = new TreeNode(3);
+    root.left.left = new TreeNode(4);
+    root.right.left = new TreeNode(5);
+    root.right.right = new TreeNode(6);
+    System.out.println("Tree Diameter: " + TreeDiameter.findDiameter(root));
+    root.left.left = null;
+    root.right.left.left = new TreeNode(7);
+    root.right.left.right = new TreeNode(8);
+    root.right.right.left = new TreeNode(9);
+    root.right.left.right.left = new TreeNode(10);
+    root.right.right.left.left = new TreeNode(11);
+    System.out.println("Tree Diameter: " + TreeDiameter.findDiameter(root));
+  }
+}
+
+
+// LeetCode : Instead of the number of nodes => find the length of the longest path between any two nodes.
+class Solution {
+    int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return max;
+    }
+
+    public int maxDepth(TreeNode root){
+
+        if(root==null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        max = Math.max(max, left+right);
+        return 1 + Math.max(left, right);
+    }
+}
