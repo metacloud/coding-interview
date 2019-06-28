255c63689737cb08fc78b1044f03fe93b34d0acb
jennachoi <juncr1719@gmail.com>
2019-06-27 18:05:10 -0700

    LeetCode 15 [Medium]

--- Coding-Interview-Patterns/02 Two Pointers/TripletSumToZero.java
+++ Coding-Interview-Patterns/02 Two Pointers/TripletSumToZero.java
@@ -11,58 +11,62 @@ Explanation: There are four unique triplets whose sum is equal to zero.
 
 # LeetCode 15. 3Sum
 [Result]
-
+Runtime: 28 ms, faster than 92.78% of Java online submissions for 3Sum.
+Memory Usage: 46.7 MB, less than 93.41% of Java online submissions for 3Sum.
 */
-
 
 import java.util.*;
 
-class TripletSumToZero { // -3 -2 -1 1 0 1 2
+class TripletSumToZero {
 
-  public static List<List<Integer>> searchTriplets(int[] arr) {
-    List<List<Integer>> result = new ArrayList<>();
+  public static List<List<Integer>> threeSum(int[] nums) {
 
-    Arrays.sort(arr);
-    for(int left = 0; left < arr.length - 2; left++){
-      if(left > 0 && arr[left] == arr[left-1]){
-        continue;
+      List<List<Integer>> threePair = new ArrayList<>();
+
+      Arrays.sort(nums);
+      for(int i = 0; i < nums.length-2; i++){
+          if( i > 0 && nums[i] == nums[i-1]){
+              continue;
+          }
+          getPairs(nums, nums[i], i+1, threePair);
       }
-      // x + y + z == 0 <=> y+z = -x : -arr[left]
-      searchPair(arr, -arr[left], left+1, result);
-    }
-    return result;
+      return threePair;
   }
 
-  private static void searchPair(int[] arr, int targetSum, int left, List<List<Integer>> result){
-      int right = arr.length-1;
-      while(left < right){
-        int targetDiff = targetSum - arr[left];
-        if(targetDiff == arr[right]){
-          //we found it!
-          result.add(Arrays.asList(-targetSum, arr[left], arr[right]));
-          left++;
-          right--;
-          while(left < right && arr[left] == arr[left-1]){
-            left++;
-          }
-          while(left < right && arr[right] == arr[right+1]){
-            right--;
-          }
-        }
-        else if(targetDiff > arr[right]){
-          left++;
-        }
-        else {
-          //targetDiff < arr[right]
-          right--;
-        }
-      }
-  }
+    private static void getPairs(int[] nums, int targetSum, int left, List<List<Integer>> threePair){
+         int right = nums.length-1;
 
+         while(left < right){
+             //targetSum(a) + b + c = 0
+             // b + c = -targetSum(a)
+             // c(right) = -targetSum(a) - b(left)
+             int target = -targetSum - nums[left];
 
+             if(target == nums[right] ){
+                 //we found it!
+                 threePair.add(Arrays.asList(targetSum, nums[left], nums[right]));
+                 //update left & right, we may have different unique pair has same targetSum.
+                 left++;
+                 right--;
+                 while(left < right && nums[left] == nums[left-1]){
+                     left++;
+                 }
+                 while(left < right && nums[right] == nums[right + 1]){
+                     right--;
+                 }
+             }
+
+             else if( target > nums[right]){
+                 left++;
+             }
+             else { // target < nums[right]
+                 right--;
+             }
+         }
+     }
 
   public static void main(String[] args) {
-    System.out.println(TripletSumToZero.searchTriplets(new int[] { -3, 0, 1, 2, -1, 1, -2 }));
-    System.out.println(TripletSumToZero.searchTriplets(new int[] { -5, 2, -1, -2, 3 }));
+    System.out.println(TripletSumToZero.threeSum(new int[] { -3, 0, 1, 2, -1, 1, -2 }));
+    System.out.println(TripletSumToZero.threeSum(new int[] { -5, 2, -1, -2, 3 }));
   }
 }
