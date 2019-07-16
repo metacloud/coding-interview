6b973bbc21385cbf7febb94839370ab9eab123ef
jennachoi <juncr1719@gmail.com>
2019-07-15 20:19:34 -0700

    LeetCode 209 [Medium]

--- Coding-Interview-Patterns/01 Sliding Window/MinSizeSubArraySum.java
+++ Coding-Interview-Patterns/01 Sliding Window/MinSizeSubArraySum.java
@@ -1,6 +1,6 @@
 /*
 Pattern: Sliding Window
-02 Smallest Subarray with a given sum (easy)
+02 Smallest Subarray with a given sum 
 
 Given an array of positive numbers and a positive number ‘S’,
 find the length of the smallest subarray whose sum is greater than or equal to ‘S’.
@@ -15,40 +15,24 @@ Memory Usage: 35.6 MB, less than 99.92% of Java online submissions for Minimum S
 // Dont forget to check Base & Edge Cases!
 class MinSizeSubArraySum {
   public static int findMinSubArray(int S, int[] arr) {
-    if(arr.length==0) return 0;
+    if(arr.length == 0) return 0;
 
-    int minLen = Integer.MAX_VALUE, Len = 0;
-    int startSub = 0, endSub = 0;
-    int sum = 0;
-
-    for(endSub = 0; endSub < arr.length; endSub++){
-        sum = sum + arr[endSub];
-        Len++;
-
+    int startPoint = 0, endPoint = 0;
+    int minLen = Integer.MAX_VALUE, sum = 0;   
+    for(endPoint = 0; endPoint < arr.length; endPoint++){
+        sum = sum + arr[endPoint];
         while(sum >= S) {
-            if(Len < minLen){
-                minLen = Len;
-            }
-            sum = sum - arr[startSub];
-            Len--;
-            startSub++;
+            minLen = Math.min(minLen, endPoint-startPoint+1);
+            sum = sum - arr[startPoint];
+            startPoint++;
         }
     }
     // Sum of all elements < S; (= No subarray >= S)
     if(minLen == Integer.MAX_VALUE) return 0;
     return minLen;
-  }
-
-  public static void main(String[] args) {
-    int result = MinSizeSubArraySum.findMinSubArray(7, new int[] { 2, 1, 5, 2, 3, 2 });
-    System.out.println("Smallest subarray length: " + result);
-    result = MinSizeSubArraySum.findMinSubArray(7, new int[] { 2, 1, 5, 2, 8 });
-    System.out.println("Smallest subarray length: " + result);
-    result = MinSizeSubArraySum.findMinSubArray(8, new int[] { 3, 4, 1, 1, 6 });
-    System.out.println("Smallest subarray length: " + result);
-  }
+  }/*
+    Time Complexity: O(N)
+    Space Complexity: O(1)
+    */
 }
-/*
-Time Complexity: O(N)
-Space Complexity: O(1)
-*/
+
