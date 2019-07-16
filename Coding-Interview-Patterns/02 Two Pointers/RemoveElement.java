363980139dfc36e1788fb5b6d039ac9480f8052e
jennachoi <juncr1719@gmail.com>
2019-07-16 15:19:30 -0700

    LeetCode 27[Easy]

--- Coding-Interview-Patterns/02 Two Pointers/RemoveElement.java
+++ Coding-Interview-Patterns/02 Two Pointers/RemoveElement.java
@@ -2,7 +2,7 @@
 Pattern: Two Pointers
 02.5 Remove Element (easy)
 
-Given an unsorted array of numbers and a target ‘key’,
+Given an unsorted array of numbers and a target ‘key’, 
 remove all instances of ‘key’ in-place and return the new length of the array.
 
 # LeetCode 27. Remove Element [Easy]
@@ -13,14 +13,14 @@ Memory Usage: 34.6 MB, less than 100.00% of Java online submissions for Remove E
 
 class RemoveElement {
   public static int remove(int[] arr, int key){
-    int rmKeyArray = 0;
+    int removedKey = 0;
     for(int i=0; i < arr.length; i++){
       if(arr[i] != key){
-        arr[rmKeyArray] = arr[i];
-        rmKeyArray++;
+        arr[removedKey] = arr[i];
+        removedKey++;
       }
     }
-    return rmKeyArray;
+    return removedKey;
   } /*
   Time Complexity: O(N)
   Space Complexity: O(1)
