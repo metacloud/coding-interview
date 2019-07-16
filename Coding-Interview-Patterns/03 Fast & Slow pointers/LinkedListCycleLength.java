9f0cb8aa2b2be4ffb7c5f74c481a91bfcfc7300f
jennachoi <juncr1719@gmail.com>
2019-07-16 12:33:15 -0700

    GeeksforGeeks [Easy]

--- Coding-Interview-Patterns/03 Fast & Slow pointers/LinkedListCycleLength.java
+++ Coding-Interview-Patterns/03 Fast & Slow pointers/LinkedListCycleLength.java
@@ -1,4 +1,3 @@
-
 /*
 Pattern: Fast & Slow pointers
 01.5 LinkedList Cycle Length (easy)
@@ -9,7 +8,7 @@ Given the head of a LinkedList with a cycle, find the length of the cycle.
 class ListNode {
   int value = 0;
   ListNode next;
-
+  
   ListNode(int value){
     this.value = value;
   }
@@ -17,7 +16,6 @@ class ListNode {
 
 class LinkedListCycleLength{
     public static int findCycleLength(ListNode head){
-
       ListNode slow = head;
       ListNode fast = head;
 
@@ -31,31 +29,17 @@ class LinkedListCycleLength{
       return 0; // no cycle
     }
 
-    public static int getCycleLength(ListNode slow){
-
-        ListNode curr = slow.next;
-        int lenCycle = 1; // Included Node of slow position
-        while(curr != slow){
-          lenCycle++;
-          curr = curr.next;
-        }
-        return lenCycle;
+    private static int getCycleLength(ListNode slow){
+        ListNode current = slow;
+        int cycleLength = 0;
+        do {
+          current = current.next;
+          cycleLength++;
+        } while(current != slow);
+  
+        return cycleLength;
     }/*
     Time Complexity: O(N)
     Space Complexity: O(1)
     */
-
-    public static void main(String[] args) {
-      ListNode head = new ListNode(1);
-      head.next = new ListNode(2);
-      head.next.next = new ListNode(3);
-      head.next.next.next = new ListNode(4);
-      head.next.next.next.next = new ListNode(5);
-      head.next.next.next.next.next = new ListNode(6);
-      head.next.next.next.next.next.next = head.next.next;
-      System.out.println("LinkedList cycle length: " + LinkedListCycleLength.findCycleLength(head));
-
-      head.next.next.next.next.next.next = head.next.next.next;
-      System.out.println("LinkedList cycle length: " + LinkedListCycleLength.findCycleLength(head));
-    }
 }
