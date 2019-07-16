442dd2671f1afdae3f07bdb5093120a7cd6eed71
jennachoi <juncr1719@gmail.com>
2019-07-16 15:40:30 -0700

    LeetCode 206[Easy]

--- Coding-Interview-Patterns/06 In-place Reversal of a LinkedList/ReverseLinkedList.java
+++ Coding-Interview-Patterns/06 In-place Reversal of a LinkedList/ReverseLinkedList.java
@@ -1,4 +1,3 @@
-
 /*
 Pattern: In-place Reversal of a LinkedList
 01 Reverse a LinkedList (easy)
@@ -12,7 +11,6 @@ Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Linked
 Memory Usage: 37.2 MB, less than 99.77% of Java online submissions for Reverse Linked List.
 */
 
-
 class ListNode {
   int value = 0;
   ListNode next;
@@ -23,39 +21,22 @@ class ListNode {
 }
 
 class ReverseLinkedList {
-
   public static ListNode reverse(ListNode head) {
     if(head==null || head.next==null) return head;
 
     ListNode current = head;
-    ListNode reverse = null;
+    ListNode reversed = null;
     ListNode temp = null;
 
     while(current != null){
       temp = current.next;
-      current.next = reverse;
-      reverse = current;
+      current.next = reversed;
+      reversed = current;
       current = temp;
     }
-
-    return reverse;
+    return reversed;
   }/*
-  Time Complexity: O(n)
+  Time Complexity: O(N)
   Space Complexity: O(1)
   */
-
-  public static void main(String[] args) {
-    ListNode head = new ListNode(2);
-    head.next = new ListNode(4);
-    head.next.next = new ListNode(6);
-    head.next.next.next = new ListNode(8);
-    head.next.next.next.next = new ListNode(10);
-
-    ListNode result = ReverseLinkedList.reverse(head);
-    System.out.print("Nodes of the reversed LinkedList are: ");
-    while (result != null) {
-      System.out.print(result.value + " ");
-      result = result.next;
-    }
-  }
 }
