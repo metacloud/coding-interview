cc4808d03cdd7c04126c8e5633aa8f4231330228
jennachoi <juncr1719@gmail.com>
2019-07-15 23:33:15 -0700

    Basic Two Pointers [Easy]

--- Coding-Interview-Patterns/02 Two Pointers/PairWithTargetSum.java
+++ Coding-Interview-Patterns/02 Two Pointers/PairWithTargetSum.java
@@ -1,29 +1,34 @@
-
 /*
 Pattern: Two Pointers
 01 Pair with Target Sum (easy)
 
-Given an array of sorted numbers and a target sum,
-find a pair in the array whose sum is equal to the given target
-Write a function to return the indices of the two numbers (i.e. the pair)
-such that they add up to the given target.
+Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to the given target
+Write a function to return the indices of the two numbers (i.e. the pair) such that they add up to the given target.
 
+[Example]
 Input: [1, 2, 3, 4, 6], target=6
 Output: [1, 3]
 Explanation: The number at index 1 and 3 add up to 6: 2+4=6
 */
-
 import java.util.HashMap;
 
 class PairWithTargetSum {
   //Two-Pointers
   public static int[] search(int[] arr, int targetSum) {
     int start = 0, end = (arr.length-1);
+    int currSum = 0;
 
     while(start < end){
-      if(arr[start]+arr[end] < targetSum) start++;
-      else if(arr[start]+arr[end] > targetSum) end--;
-      else return new int[] {start, end};
+      currSum = arr[start] + arr[end];
+      if(currSum == targetSum){
+        return new int[] {start, end};
+      }
+      else if(currSum < targetSum){
+        start++;
+      }
+      else { //currSum > targetSum
+        end--;
+      }
     }
     return new int[] {-1,-1};
   }/*
@@ -31,16 +36,15 @@ class PairWithTargetSum {
   Space Complexity: O(1)
   */
 
-
   //Hash-Table
   public static int[] searchHash(int[] arr, int targetSum){
-    HashMap<Integer,Integer> target = new HashMap<Integer, Integer>();
+    HashMap<Integer,Integer> hashmap = new HashMap<Integer, Integer>();
     for(int i=0; i < arr.length; i++){
-      if(target.containsKey(targetSum - arr[i])){
-        int getPair = target.get(targetSum-arr[i]);
-        return new int[] {getPair, i};
+      int target = targetSum - arr[i];
+      if(hashmap.containsKey(target)){
+        return new int[] {hashmap.get(target), i};
       } else {
-        target.put(arr[i], i);
+        hashmap.put(arr[i], i);
       }
     }
     return new int[] {-1, -1};
@@ -48,15 +52,4 @@ class PairWithTargetSum {
   Time Complexity: O(N)
   Space Complexity: O(N); pushing N numbers in the HashTable.
   */
-
-  public static void main(String[] args) {
-    int[] result = PairWithTargetSum.search(new int[] { 1, 2, 3, 4, 6 }, 6);
-    System.out.println("Pair with target sum: [" + result[0] + ", " + result[1] + "]");
-    result = PairWithTargetSum.search(new int[] { 2, 5, 9, 11 }, 11);
-    System.out.println("Pair with target sum: [" + result[0] + ", " + result[1] + "]");
-    result = PairWithTargetSum.searchHash(new int[] { 1, 2, 3, 4, 6 }, 6);
-    System.out.println("Pair with target sum: [" + result[0] + ", " + result[1] + "]");
-    result = PairWithTargetSum.searchHash(new int[] { 2, 5, 9, 11 }, 11);
-    System.out.println("Pair with target sum: [" + result[0] + ", " + result[1] + "]");
-  }
 }
