22d065d4672c3e99ecd712f155702f372727058a
jennachoi <juncr1719@gmail.com>
2019-07-16 20:03:01 -0700

    LeetCode 704

--- Coding-Interview-Patterns/11 Modified Binary Search/BinarySearch.java
+++ Coding-Interview-Patterns/11 Modified Binary Search/BinarySearch.java
@@ -15,25 +15,26 @@ Memory Usage: 39.4 MB, less than 94.49% of Java online submissions for Binary Se
 
 class BinarySearch {
 	public static int search(int[] arr, int key){
-		//Index value
-		int start = 0;
-		int end = arr.length-1;
-		int mid = start + (end-start)/2;
+		int start = 0, end = arr.length-1;
+		boolean isAscending = arr[start] < arr[end];
 
-		if(arr[start] < arr[end]){//ascending
-			while(start <= end){
-				mid = start + (end-start)/2;
-				if(arr[mid]==key) return mid;
-				else if(key<arr[mid]) end=mid-1;
-				else start = mid+1;
-			}
-		}
-		else {//descending
-			while(start<=end){
-				mid = start + (end-start)/2;
-				if(arr[mid]==key) return mid;
-				else if(key < arr[mid]) start = mid+1;
-				else end = mid-1;
+		while(start <= end){
+			int mid = start + (end-start)/2;
+			if(arr[mid] == key) return mid;
+
+			if(isAscending){ // ascending order
+				if(arr[mid] > key){
+					end = mid-1;
+				} else {
+					start = mid+1;
+				}
+			} 
+			else { // descending order
+				if(arr[mid] > key){
+					start = mid+1;
+				} else {
+					end = mid-1;
+				}
 			}
 		}
 		return -1;
@@ -41,11 +42,4 @@ class BinarySearch {
 	Time Complexity: O(logN); reducing the search range by half at every step.
 	Space Complexity: O(1)
 	*/
-
-	public static void main(String[] args){
-		System.out.println(BinarySearch.search(new int[] { 4, 6, 10 }, 10));
-    System.out.println(BinarySearch.search(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 5));
-    System.out.println(BinarySearch.search(new int[] { 10, 6, 4 }, 10));
-    System.out.println(BinarySearch.search(new int[] { 10, 6, 4 }, 4));
-	}
 }
