22d07fc882e20cccf6c0c1191cec1c37bd9ea954
jennachoi <juncr1719@gmail.com>
2019-07-17 16:43:27 -0700

    LeetCode 102 [Medium]

--- Coding-Interview-Patterns/07 Tree BFS/LevelOrderTraversal.java
+++ Coding-Interview-Patterns/07 Tree BFS/LevelOrderTraversal.java
@@ -1,4 +1,3 @@
-
 /*
 Pattern: Tree BFS
 01 Binary Tree Level Order Traversal (easy)
@@ -8,12 +7,9 @@ You should populate the values of all nodes of each level from left to right in 
 
 # LeetCode 102. Binary Tree Level Order Traversal [Medium]
 [Result]
-Runtime: 1 ms, faster than 76.80% of Java online submissions for Binary Tree Level Order Traversal.
-Memory Usage: 36.3 MB, less than 99.98% of Java online submissions for Binary Tree Level Order Traversal.
+Runtime: 1 ms, faster than 85.33% of Java online submissions for Binary Tree Level Order Traversal.
+Memory Usage: 36.2 MB, less than 99.98% of Java online submissions for Binary Tree Level Order Traversal.
 */
-
-
-import java.util.*;
 
 class TreeNode {
   int val;
@@ -26,8 +22,8 @@ class TreeNode {
 };
 
 class LevelOrderTraversal {
-  public static List<List<Integer>> traverse(TreeNode root){
-    List<List<Integer>> result = new ArrayList<List<Integer>>();
+  public List<List<Integer>> traverse(TreeNode root){
+    List<List<Integer>> result = new ArrayList<>();
     if(root==null) return result;
 
     Queue<TreeNode> queue = new LinkedList<>();
@@ -40,35 +36,18 @@ class LevelOrderTraversal {
       for(int i=0; i<nodesLevel; i++){
           TreeNode currentNode = queue.poll();
           currentLevel.add(currentNode.val);
-
-          if(currentNode.left != null){
-            queue.offer(currentNode.left);
-          }
-          if(currentNode.right != null){
-            queue.offer(currentNode.right);
-          }
+          if(currentNode.left != null) queue.offer(currentNode.left);
+          if(currentNode.right != null) queue.offer(currentNode.right);
       }
       result.add(currentLevel);
     }
+
     return result;
   }/*
   Time Complexity: O(N)
   Space Complexity: O(N)
   */
-
-
-  public static void main(String[] args) {
-    TreeNode root = new TreeNode(12);
-    root.left = new TreeNode(7);
-    root.right = new TreeNode(1);
-    root.left.left = new TreeNode(9);
-    root.right.left = new TreeNode(10);
-    root.right.right = new TreeNode(5);
-    List<List<Integer>> result = LevelOrderTraversal.traverse(root);
-    System.out.println("Level order traversal: " + result);
-  }
 }
-
 
 // Using Recursion
 class Solution {
