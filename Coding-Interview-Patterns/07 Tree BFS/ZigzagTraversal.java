3e205bdf289ebf1c1c3177cc2636dcb3cdf72598
jennachoi <juncr1719@gmail.com>
2019-07-17 16:42:27 -0700

    LeetCode 103 [Medium]

--- Coding-Interview-Patterns/07 Tree BFS/ZigzagTraversal.java
+++ Coding-Interview-Patterns/07 Tree BFS/ZigzagTraversal.java
@@ -6,12 +6,6 @@ Given a binary tree, populate an array to represent its zigzag level order trave
 You should populate the values of all nodes of the first level from left to right,
 then right to left for the next level and keep alternating in the same manner for the following levels.
 
-     1
-   /   \
-  2     3
- / \   / \
-4  5  6   7   Zigzag Level Order Traversal: [[1], [3,2], [4,5,6,7]]
-
 # LeetCode 103. Binary Tree Zigzag Level Order Traversal [Medium]
 [Result]
 Runtime: 1 ms, faster than 88.41% of Java online submissions for Binary Tree Zigzag Level Order Traversal.
@@ -24,33 +18,35 @@ class TreeNode {
      TreeNode(int x) { val = x; }
  }
 
-class Solution {
-    public List< List<Integer>> zigzagLevelOrder(TreeNode root) {
+class ZigzagTraversal {
+    public List< List<Integer>> traverse(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
         if(root==null) return result;
 
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-        
-        boolean reversed = false;
+        boolean fromLeft = false;
         while(!queue.isEmpty()){
             int levelSize = queue.size();
             List<Integer> levelNodes = new ArrayList<>();
             for(int i=0; i<levelSize; i++){
                 TreeNode temp = queue.poll();
-                if(reversed){ 
-                  levelNodes.add(0, temp.val);
-                } else { // reversed == false
-                  levelNodes.add(temp.val);
-                }
                 if(temp.left != null) queue.offer(temp.left);
                 if(temp.right != null)  queue.offer(temp.right);
+                if(fromLeft) {
+                    levelNodes.add(0, temp.val);
+                } else { // fromLeft == false
+                  levelNodes.add(temp.val);
+                }
             }
-            if(reversed){
-             reversed = false;
-            } else reversed = true;
+            if(fromLeft) fromLeft = false;
+            else fromLeft = true;
+            
             result.add(levelNodes);
         }
         return result;
     }
-}
+}/*
+Time Complexity: O(N)
+Space Complexity: O(N)
+*/
