4640f25ea6867e213fac89bda8115f1834f18017
jennachoi <juncr1719@gmail.com>
2019-07-16 20:03:40 -0700

    LeetCode 704

--- Coding-Interview-Patterns/11 Modified Binary Search/BinarySearch.java
+++ Coding-Interview-Patterns/11 Modified Binary Search/BinarySearch.java
@@ -14,32 +14,32 @@ Memory Usage: 39.4 MB, less than 94.49% of Java online submissions for Binary Se
 */
 
 class BinarySearch {
-	public static int search(int[] arr, int key){
-		int start = 0, end = arr.length-1;
-		boolean isAscending = arr[start] < arr[end];
+    public static int search(int[] arr, int key){
+        int start = 0, end = arr.length-1;
+        boolean isAscending = arr[start] < arr[end];
 
-		while(start <= end){
-			int mid = start + (end-start)/2;
-			if(arr[mid] == key) return mid;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(arr[mid] == key) return mid;
 
-			if(isAscending){ // ascending order
-				if(arr[mid] > key){
-					end = mid-1;
-				} else {
-					start = mid+1;
-				}
-			} 
-			else { // descending order
-				if(arr[mid] > key){
-					start = mid+1;
-				} else {
-					end = mid-1;
-				}
-			}
-		}
-		return -1;
-	}/*
-	Time Complexity: O(logN); reducing the search range by half at every step.
-	Space Complexity: O(1)
-	*/
+            if(isAscending){ // ascending order
+                if(arr[mid] > key){
+                    end = mid-1;
+                } else {
+                    start = mid+1;
+                }
+            } 
+            else { // descending order
+                if(arr[mid] > key){
+                    start = mid+1;
+                } else {
+                    end = mid-1;
+                }
+            }
+        }
+        return -1;
+    }/*
+    Time Complexity: O(logN); reducing the search range by half at every step.
+    Space Complexity: O(1)
+    */
 }
