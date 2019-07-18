89eceb71893df7dac4b704989ea96d9dc5a91dbd
jennachoi <juncr1719@gmail.com>
2019-07-17 19:19:26 -0700

    LeetCode 112 [Easy]

--- Coding-Interview-Patterns/08 Tree DFS/TreePathSum.java
+++ Coding-Interview-Patterns/08 Tree DFS/TreePathSum.java
@@ -1,5 +1,3 @@
-
-
 /*
 Pattern: Tree DFS
 01 Binary Tree Level Order Traversal (easy)
@@ -12,8 +10,7 @@ find if the tree has a path from root-to-leaf such that the sum of all the node 
 Runtime: 0 ms, faster than 100.00% of Java online submissions for Path Sum.
 Memory Usage: 37.4 MB, less than 98.84% of Java online submissions for Path Sum.
 */
-
-
+ 
 class TreeNode {
   int val;
   TreeNode left;
@@ -26,24 +23,12 @@ class TreeNode {
 
 class TreePathSum {
   public static boolean hasPath(TreeNode root, int sum) {
-
     if(root == null) return false;
     if(root.val == sum && root.left == null && root.right == null) return true;
 
     return hasPath(root.left, sum - root.val) || hasPath(root.right, sum - root.val);
   }/*
   Time Complexity: O(N)
-  Space Complexity: O(N)
+  Space Complexity: O(N) for recursion stack
   */
-
-  public static void main(String[] args) {
-    TreeNode root = new TreeNode(12);
-    root.left = new TreeNode(7);
-    root.right = new TreeNode(1);
-    root.left.left = new TreeNode(9);
-    root.right.left = new TreeNode(10);
-    root.right.right = new TreeNode(5);
-    System.out.println("Tree has path: " + TreePathSum.hasPath(root, 23));
-    System.out.println("Tree has path: " + TreePathSum.hasPath(root, 16));
-  }
 }
