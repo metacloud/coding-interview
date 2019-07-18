0dd89a105f2c8d9d7b0db944a6efc9ae7b2ec173
jennachoi <juncr1719@gmail.com>
2019-07-18 14:18:19 -0700

    LeetCode 21 [Easy]

---
+++ LeetCode/Top 100 Liked Questions/01 Easy/MergeTwoSortedLists.java
@@ -0,0 +1,28 @@
+/*
+LeetCode 21. Merge Two Sorted Lists
+
+[Result]
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Merge Two Sorted Lists.
+Memory Usage: 40.8 MB, less than 10.44% of Java online submissions for Merge Two Sorted Lists.
+*/
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        ListNode prehead = new ListNode(-1);
+        ListNode prev = prehead;
+        
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        prev.next = l1 == null ? l2 : l1;
+        return prehead.next;
+    }
+}
+
