7d54b99fc03fcdd9422c98347f89b506ecd9649c
jennachoi <juncr1719@gmail.com>
2019-07-17 21:34:52 -0700

    LeetCode 437 [Easy]

--- Coding-Interview-Patterns/08 Tree DFS/CountAllPathSum.java
+++ Coding-Interview-Patterns/08 Tree DFS/CountAllPathSum.java
@@ -1,4 +1,3 @@
-
 /*
 Pattern: Tree DFS
 05 Count Paths for a Sum (medium)
@@ -10,8 +9,7 @@ Please note that the paths can start or end at any node but all paths must follo
     /  \
    7    9
  /  \  / \
- 6  5 2  3
-
+ 6  5 2   3
 Sum = 12, Output = 3
 3 paths with sum '12' : 7->5 , 1->9->2, 9->3
 
@@ -19,8 +17,6 @@ Sum = 12, Output = 3
 Runtime: 11 ms, faster than 46.58% of Java online submissions for Path Sum III.
 Memory Usage: 40.4 MB, less than 69.31% of Java online submissions for Path Sum III.
 */
-
-import java.util.*;
 
 class TreeNode {
   int val;
@@ -33,29 +29,16 @@ class TreeNode {
 };
 
 class CountAllPathSum {
-  public static int countPaths(TreeNode root, int S) {
+  public int countPaths(TreeNode root, int Sum) {
     if(root == null) return 0;
-    return trackPaths(root, S, 0) + countPaths(root.left, S) + countPaths(root.right, S);
+    return recursiveDFS(root, Sum) + countPaths(root.left, Sum) + countPaths(root.right, Sum);
   }
 
-  private static int trackPaths (TreeNode root, int sum, int currSum){
+  private int recursiveDFS (TreeNode root, int sum){
       if(root == null) return 0;
-
-      currSum = currSum + root.val;
-      if(currSum == sum){
-        return 1+ trackPaths(root.left, sum, currSum) + trackPaths(root.right, sum, currSum);
-      }
-      return trackPaths(root.left, sum, currSum) + trackPaths(root.right, sum, currSum);
+      return (root.val == sum ? 1 : 0) + recursiveDFS(root.left, sum - root.val) + recursiveDFS(root.right, sum - root.val);
   }
-
-
-  public static void main(String[] args) {
-    TreeNode root = new TreeNode(12);
-    root.left = new TreeNode(7);
-    root.right = new TreeNode(1);
-    root.left.left = new TreeNode(4);
-    root.right.left = new TreeNode(10);
-    root.right.right = new TreeNode(5);
-    System.out.println("Tree has path: " + CountAllPathSum.countPaths(root, 11));
-  }
-}
+}/*
+Time Complexity: worst = O(N^2) for skewed tree, best = O(NlogN) for balanced tree
+Space Complexity: O(N)
+*/
