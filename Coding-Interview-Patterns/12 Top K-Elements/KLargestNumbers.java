7f7523af88b3813133bdff1c78f37603f9cc33b7
jennachoi <juncr1719@gmail.com>
2019-07-18 23:11:48 -0700

    GeeksforGeeks [Easy]

--- Coding-Interview-Patterns/12 Top K-Elements/KLargestNumbers.java
+++ Coding-Interview-Patterns/12 Top K-Elements/KLargestNumbers.java
@@ -5,20 +5,15 @@ Pattern: Top 'K' Elements
 Given an unsorted array of numbers, find the ‘K’ largest numbers in it.
 Note: For a detailed discussion about different approaches to solve this problem, take a look at Kth Smallest Number.
 
-Example 1: Input: [3, 1, 5, 12, 2, 11], K = 4
-           Output: [3, 11, 5 ,12]
-Example 2: Input: [5, 12, 11, -1, 12], K = 3
-           Output: [12, 11, 12]
-*/
-
-import java.util.*;
+Input: [3, 1, 5, 12, 2, 11], K = 4
+Output: [3, 11, 5 ,12]
 /*
 Brute force Solution: sorting the array and then return the largest K numbers.
 Time Complexity: O(NlogN)
 */
 
 class KLargestNumbers {
-  public static List<Integer> findKLargestNumbers(int[] nums, int k){
+  public List<Integer> findKLargestNumbers(int[] nums, int k){
     //minHeap
     PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
     for(int i=0; i<k; i++){
@@ -36,13 +31,4 @@ class KLargestNumbers {
   Time Complexity: O(K + (N-K)*logK) ( better than O(N*logN) )
   Space Complexity: O(K)
   */
-
-
-  public static void main(String[] args) {
-      List<Integer> result = KLargestNumbers.findKLargestNumbers(new int[] { 3, 1, 5, 12, 2, 11 }, 3);
-      System.out.println("Here are the top K numbers: " + result);
-
-      result = KLargestNumbers.findKLargestNumbers(new int[] { 5, 12, 11, -1, 12 }, 3);
-      System.out.println("Here are the top K numbers: " + result);
-  }
 }
