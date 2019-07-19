5e3e72bf579457532dbe67249d65529587556b20
jennachoi <juncr1719@gmail.com>
2019-07-18 23:18:23 -0700

    GeeksforGeeks [Easy]

--- Coding-Interview-Patterns/12 Top K-Elements/KthSmallestNumber.java
+++ Coding-Interview-Patterns/12 Top K-Elements/KthSmallestNumber.java
@@ -13,32 +13,31 @@ Explanation: The 3rd smallest number is '5', as the first two smaller numbers ar
 */
 
 class KthSmallestNumber {
-      public int findKthSmallestNumber(int[] nums, int k){
-        //maxHeap
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)->Integer.compare(b,a));
-        for(int i=0; i<k; i++){
-          maxHeap.add(nums[i]);
-        }
+  public int findKthSmallestNumber(int[] nums, int k){
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>((a,b)->(b-a));
+    for(int i=0; i<k; i++){
+      maxHeap.add(nums[i]);
+    }
 
-        for(int i=k; i<nums.length; i++){
-          if(maxHeap.peek() > nums[i]){
-            maxHeap.poll();
-            maxHeap.add(nums[i]);
-          }
-        }
-        return maxHeap.peek();
-      }/*
-      Time Complexity: O(K + (N-K)*logK)
-      Space Complexity: O(K)
-      */
-
-      /*
-      Alternate Solution : we can use a Min Heap to find the Kth smallest number.
-      we are able to insert all the numbers in the min-heap, then get the top 'K' Numbers
-      from the heap to find the Kth smallest number.
-      -Inserting all numbers in the heap : O(N)
-      -Extracting 'K' numbers : O(KlogN)
-      : Time Complexity: O(N+KlogN)
-      : Space Complexity: O(N)
-      */
+    for(int i=k; i<nums.length; i++){
+      if(maxHeap.peek() > nums[i]){
+        maxHeap.poll();
+        maxHeap.add(nums[i]);
+      }
+    }
+    return maxHeap.peek();
+  }/*
+  Time Complexity: O(K + (N-K)*logK)
+  Space Complexity: O(K)
+  */
 }
+
+/*
+  Alternate Solution : we can use a Min Heap to find the Kth smallest number.
+  we are able to insert all the numbers in the min-heap, then get the top 'K' Numbers
+  from the heap to find the Kth smallest number.
+  -Inserting all numbers in the heap : O(N)
+  -Extracting 'K' numbers : O(KlogN)
+  : Time Complexity: O(N+KlogN)
+  : Space Complexity: O(N)
+*/
