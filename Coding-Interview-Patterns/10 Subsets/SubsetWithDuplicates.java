e3f30189089bcc403c00f960fc8207373274c091
jennachoi <juncr1719@gmail.com>
2019-07-19 23:57:15 -0700

    LeetCode 90 [Medium]

--- Coding-Interview-Patterns/10 Subsets/SubsetWithDuplicates.java
+++ Coding-Interview-Patterns/10 Subsets/SubsetWithDuplicates.java
@@ -12,10 +12,8 @@ Runtime: 1 ms, faster than 100.00% of Java online submissions for Subsets II.
 Memory Usage: 36.4 MB, less than 99.28% of Java online submissions for Subsets II.
 */
 
-import java.util.*;
-
 class SubsetWithDuplicates {
-  public static List<List<Integer>> findSubsets(int[] nums){
+  public List<List<Integer>> findSubsets(int[] nums){
     List<List<Integer>> subsets = new ArrayList<>();
     subsets.add(new ArrayList<>());
 
@@ -41,12 +39,4 @@ class SubsetWithDuplicates {
   Space Complexity: O(2^N)
   ; for output list
   */
-
-  public static void main(String[] args){
-    List<List<Integer>> result = SubsetWithDuplicates.findSubsets(new int[] { 1, 3, 3 });
-    System.out.println("Here is the list of subsets: " + result);
-
-    result = SubsetWithDuplicates.findSubsets(new int[] { 1, 5, 3, 3 });
-    System.out.println("Here is the list of subsets: " + result);
-  }
 }
