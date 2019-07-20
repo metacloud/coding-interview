5103c8e72f4a4626a7a6e90492c495a1bfd91f4f
jennachoi <juncr1719@gmail.com>
2019-07-19 23:36:44 -0700

    LeetCode 78 [Medium]

--- Coding-Interview-Patterns/10 Subsets/Subsets.java
+++ Coding-Interview-Patterns/10 Subsets/Subsets.java
@@ -10,15 +10,12 @@ Runtime: 0 ms, faster than 100.00% of Java online submissions for Subsets.
 Memory Usage: 36.6 MB, less than 98.93% of Java online submissions for Subsets.
 */
 
-
-import java.util.*;
-
 class Subsets {
-  public static List<List<Integer>> findSubsets(int[] nums){
+  public List<List<Integer>> findSubsets(int[] nums){
     List<List<Integer>> subsets = new ArrayList<>();
-
     //add EmptySet
     subsets.add(new ArrayList<>());
+
     for(int num : nums){
         int n = subsets.size();
         for(int i=0; i<n; i++){
@@ -36,12 +33,4 @@ class Subsets {
   Space Complexity: O(2^N)
   ; all the additional space used is for the output list.
   */
-
-  public static void main(String[] args) {
-    List<List<Integer>> result = Subsets.findSubsets(new int[] { 1, 3 });
-    System.out.println("Here is the list of subsets: " + result);
-
-    result = Subsets.findSubsets(new int[] { 1, 5, 3 });
-    System.out.println("Here is the list of subsets: " + result);
-  }
 }
