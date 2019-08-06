4ec6b284c2547c36cd03277dedd24dc82a8226d8
jennachoi <juncr1719@gmail.com>
2019-08-05 22:58:26 -0700

    DFS: N-ary Tree [LC1120]

--- LeetCode/MockInterview/A16-SubtreeWithMaximumAverage.java
+++ LeetCode/MockInterview/A16-SubtreeWithMaximumAverage.java
@@ -1,42 +1,41 @@
 /*
-1120. Maximum Average Subtree
+Subtree with Maximum Average
 */
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+/*
+Given an N-ary tree, find the subtree with maximum average. Return the root of the subtree.
+A subtree of a tree is any node of that tree plus all its descendants.
+The average value of a subtree is the sum of its values, divided by the number of nodes.
+*/
+
 class Solution {
-    private class node {
-        int numNodes;
-        int sum; 
-        node(int numNodes, int sum){
-            this.numNodes = numNodes;
-            this.sum = sum;
+    double max = Integer.MIN_VALUE;
+    TreeNode ret = null;
+    public TreeNode maximumAverageSubtree(TreeNode root) {
+        if(root==null) return ret;
+        dfsHelper(root);
+        return ret;
+    }
+
+    private double[] dfsHelper(TreeNode root){
+        if(root==null) return new double[] {0,0};
+
+        double total = root.val;
+        int nodeCount = 1;
+        for(TreeNode node : root.children){
+            double[] ret = dfsHelper(node);
+            total += ret[0];
+            nodeCount += ret[1];
         }
+        double avg = total/nodeCount;
+        if(avg > max){
+            max = avg;
+            ret = root;
+        }
+
+        return new double[] {total, nodeCount};
     }
-    
-    double max = 0.0;
-    public double maximumAverageSubtree(TreeNode root) { 
-        getMaxAvg(root);        
-        return max;
-    }
-    
-    private node getMaxAvg(TreeNode root){
-        if(root==null) return new node(0,0);
-        
-        node left = getMaxAvg(root.left);
-        node right = getMaxAvg(root.right);
-        int numNodes = left.numNodes + right.numNodes + 1;
-        int totalSum = left.sum + right.sum + root.val;
-        
-        max = Math.max(max, (double)totalSum/numNodes);
-        
-        return new node(numNodes, totalSum);   
-    }  
-}
+}/*
+Time Complexity: O(N)
+Space Complexity: O(1)
+*/
