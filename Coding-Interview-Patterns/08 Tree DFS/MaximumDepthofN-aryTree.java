a3bc5a669d2c152c160826401ac6c2713e5de35a
jennachoi <juncr1719@gmail.com>
2019-08-05 20:04:56 -0700

    LeetCode 559 [Easy]

---
+++ Coding-Interview-Patterns/08 Tree DFS/MaximumDepthofN-aryTree.java
@@ -0,0 +1,31 @@
+/*
+559. Maximum Depth of N-ary Tree
+*/
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public int maxDepth(Node root) {
+        if(root==null) return 0;
+        
+        int max = 0;
+        for(Node node : root.children){
+            int depth = maxDepth(node); // 1
+            max = Math.max(max, depth);                        
+        }
+        return max+1;
+        
+    }
+}
