38e452937c95676b92571be0c517d46283a94e0b
jennachoi <juncr1719@gmail.com>
2019-08-09 13:33:53 -0700

    String: LeetCode 763 [Medium]

--- LeetCode/MockInterview/A05-PartitionLabels.java
+++ LeetCode/MockInterview/A05-PartitionLabels.java
@@ -4,21 +4,21 @@
 
 class Solution {
     public List<Integer> partitionLabels(String S) {
-        
+
         // total alphabet : 26
-        int[] lastIndex = new int[26]; 
-        
+        int[] lastIndex = new int[26];
+
         // each Index: alphabet-'a' will have their alphabet's last index position
-        for(int i=0; i<S.length(); i++){ 
+        for(int i=0; i<S.length(); i++){
             lastIndex[S.charAt(i)-'a'] = i;
         }
-        
+
         List<Integer> ret = new ArrayList<Integer>();
-        
+
         int currLast=0, start=0;
         for(int i=0; i<S.length(); i++){
             currLast = Math.max(currLast, lastIndex[S.charAt(i)-'a']);
-            
+
             if(i==currLast){ //find one complete part
                 ret.add(currLast-start+1);
                 start=currLast+1;
@@ -26,4 +26,9 @@ class Solution {
         }
         return ret;
     }
-}
+}/*
+Time Complexity: O(N)
+Space Complexity: O(1)
+=> alphabet:26 ; O(26)~= O(1) 
+If the size(space) does not increase in size with the respect to input N, then it is constant space, O(1)
+*/
