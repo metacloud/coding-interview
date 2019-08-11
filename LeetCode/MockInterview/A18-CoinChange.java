eb02a5808f3ad30b47077ed85b01f67be9500b30
jennachoi <juncr1719@gmail.com>
2019-08-10 22:25:18 -0700

    DP: LeetCode 322 [Medium]

---
+++ LeetCode/MockInterview/A18-CoinChange.java
@@ -0,0 +1,26 @@
+/*
+322. Coin Change
+
+Like an unbounded knapsack.
+*/
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, amount+1);
+
+        dp[0] = 0;
+        for(int i=0; i<=amount; i++){
+            for(int j=0; j<coins.length; j++){
+                if(coins[j] <= i){
+                    dp[i] = Math.min(dp[i], 1+dp[i-coins[j]]); //1 : current coin count
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}/*
+Time Complexity: O(N*M) where N is the amount, M is the number of coins.
+Space Complexity: O(N) for the memoization table.
+*/
