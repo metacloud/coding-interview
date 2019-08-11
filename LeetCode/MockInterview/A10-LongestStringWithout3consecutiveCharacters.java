efa6ebdf02ab2393dccb345c0a65ddf47756a33d
jennachoi <juncr1719@gmail.com>
2019-08-11 15:56:24 -0700

    String/Greedy Problem [Medium]

--- LeetCode/MockInterview/A10-LongestStringWithout3consecutiveCharacters.java
+++ LeetCode/MockInterview/A10-LongestStringWithout3consecutiveCharacters.java
@@ -3,7 +3,8 @@ Longest string without 3 consecutive characters
 */
 
 /*
-Given A, B, C, find any string of maximum length that can be created such that no 3 consecutive characters are same. There can be at max A 'a', B 'b' and C 'c'.
+Given A, B, C, find any string of maximum length that can be created such that no 3 consecutive characters are same.
+There can be at max A 'a', B 'b' and C 'c'.
 
 Input: A = 1, B = 1, C = 6
 Output: "ccbccacc"
@@ -13,61 +14,49 @@ https://leetcode.com/problems/distant-barcodes
 https://leetcode.com/problems/rearrange-string-k-distance-apart (premium)
 */
 
+
 class Solution {
-    public class Pair {
+    public class Node {
         char ch;
         int cnt;
-        Pair(char ch, int cnt){
+        Node(char ch, int cnt){
             this.ch = ch;
             this.cnt = cnt;
         }
     }
 
-    public String longestString(int A, int B, int C){
+    public String maxLenString(int A, int B, int C){
 
-        Pair a = new Pair('a', A);
-        Pair b = new Pair('b', B);
-        Pair c = new Pair('c', C);
+        Node nodeA = new Node('a', A);
+        Node nodeB = new Node('b', B);
+        Node nodeC = new Node('c', C);
 
-        PriorityQueue<Pair> maxHeap = new PriorityQueue<>(new Comparator<Pair>(){
+        PriorityQueue<Node> pq = new PriorityQueue<>(new Comparator<Node>(){
             @Override
-            public int compare(Pair a, Pair b){
-                return b.cnt-a.cnt !=0 ? b.cnt-a.cnt : a.ch-b.ch;
+            public int compare(Node a, Node b){
+                return (b.cnt-a.cnt!=0 ? b.cnt-a.cnt : a.ch-b.ch);
             }
         });
 
-        maxHeap.offer(a);
-        maxHeap.offer(b);
-        maxHeap.offer(c);
+        pq.offer(nodeA); pq.offer(nodeB); pq.offer(nodeC);
 
         StringBuilder sb = new StringBuilder();
-        Queue<Pair> queue = new LinkedList<>();
-        while(!maxHeap.isEmpty()){
-            int count=0;
-            while(count<3 && !maxHeap.isEmpty()){
-                Pair temp = maxHeap.poll();
-                if(temp.cnt >= 2){
-                    sb.append(temp.ch);
-                    sb.append(temp.ch);
-                    temp.cnt--;
-                    temp.cnt--;
-                    count++;
-                    count++;
-                    if(temp.cnt>0) queue.offer(temp);
-                } else { //cnt==1
-                    sb.append(temp.ch);
-                    temp.cnt--;
-                    count++;
-                }
+        Queue<Node> queue = new LinkedList<>();
+        while(!pq.isEmpty()){
+            Node curr = pq.poll();
+            sb.append(curr.ch);
+            curr.cnt--;
+            if(curr.cnt>0){
+                sb.append(curr.ch);
+                curr.cnt--;
             }
 
-            //it means we just added same char twice, and the char in the queue is the same one we just added.
-            if(count<3 && queue.size()>0) return "";
-            while(!queue.isEmpty()){
-                maxHeap.offer(queue.poll());
+            queue.offer(curr);
+            if(queue.size()==2){//k==2
+                Node temp = queue.poll();
+                if(temp.cnt>0) pq.offer(temp);
             }
         }
-
         return sb.toString();
     }
 }
