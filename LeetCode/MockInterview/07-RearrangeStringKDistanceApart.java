872cffe153d98b1a91200000aed6c49b72d9fb08
jennachoi <juncr1719@gmail.com>
2019-08-11 15:01:59 -0700

    String/Greedy LeetCode 358 [Medium]

--- LeetCode/MockInterview/07-RearrangeStringKDistanceApart.java
+++ LeetCode/MockInterview/07-RearrangeStringKDistanceApart.java
@@ -15,24 +15,31 @@ class Solution {
     }
 
     public String rearrangeString(String str, int k){
-        if(k<=1 || str.length() < 2) return str;
+        //base check
+        if(str==null || str.length()==0) return "";
+        if(k<=1) return str;
 
-        Map<Character, Integer> map = new HashMap<>();
-        for(char ch : str.toCharArray()) map.put(ch, map.getOrDefault(ch,0)+1);
+        //check all chars frequency
+        int[] count = new int[26];
+        for(char ch : str.toCharArray()) count[ch-'a']++;
 
-        PriorityQueue<Pair> maxHeap = new PriorityQueue<>(new Comparator<Pair>(){
+        PriorityQueue<Pair> pq = new PriorityQueue<>(new Comparator<Pair>(){
             @Override
             public int compare(Pair a, Pair b){
                 return b.cnt-a.cnt != 0 ? b.cnt-a.cnt : a.ch-b.ch;
             }
         });
 
-        for(char ch : map.keySet()) maxHeap.offer(new Pair(ch, map.get(ch)));
+        //add all to pq
+        for(int i=0; i<count.length; i++){
+            if(count[i]>0) pq.offer(new Pair((char)(i+'a'), count[i]));
+        }
 
         StringBuilder sb = new StringBuilder();
         Queue<Pair> queue = new LinkedList<>();
-        while(!maxHeap.isEmpty()){
-            Pair temp = maxHeap.poll();
+
+        while(!pq.isEmpty()){
+            Pair temp = pq.poll();
             sb.append(temp.ch);
             temp.cnt--;
             queue.offer(temp);
@@ -40,39 +47,10 @@ class Solution {
             if(queue.size()==k){
                 Pair tmp = queue.poll();
                 if(tmp.cnt > 0){
-                    maxHeap.offer(tmp);
+                    pq.offer(tmp);
                 }
             }
         }
         return sb.length()==str.length() ? sb.toString() : "";
     }
-}/*
-Time Complexity:
-O(N*logN) where N is the number of characters in the input string
-but input only contains at most 26 entries(lower case characters) => O(26log26) ~= O(N)
-
-Space Complexity: O(N)
-*/
-
-
-
-/* Solution2
-
-while(!maxHeap.isEmpty()){
-    Queue<Pair> queue = new LinkedList<>();
-    for(int i=0; i<k; i++){
-        if(!maxHeap.isEmpty()){
-            Pair temp = maxHeap.poll();
-            sb.append(temp.ch);
-            if(--temp.cnt>0) queue.offer(temp);
-        } else {
-            if(queue.size()==0) return sb.toString();
-            return "";
-        }
-    }
-    for(Pair n : queue){
-        maxHeap.offer(n);
-    }
 }
-return sb.toString();
-*/
