052fb2b92f13eb3e62cd08dc4dd4336b2be1d5a5
jennachoi <juncr1719@gmail.com>
2019-08-11 22:02:38 -0700

    DFS/Matrix LeetCode 200 [Medium]

--- LeetCode/MockInterview/02-NumberofIslands.java
+++ LeetCode/MockInterview/02-NumberofIslands.java
@@ -4,31 +4,31 @@
 
 class Solution {
     public int numIslands(char[][] grid) {
-        int numIslands=0;  
+        int numIslands = 0;
         if(grid==null || grid.length==0 || grid[0].length==0) return numIslands;
-        
+
         for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[i].length;j++){
-                if(grid[i][j] != '1'){
+            for(int j=0; j<grid[i].length; j++){
+                if(grid[i][j]!='1'){
                     continue;
                 }
                 numIslands++;
-                DFS(grid, i, j);
+                dfsHelper(grid, i, j);
             }
         }
         return numIslands;
     }
-    
-    public void DFS(char[][] grid, int i, int j){
-        grid[i][j] = 'A';
 
-        //top
-        if(i-1 >= 0 && grid[i-1][j]=='1') DFS(grid, i-1, j);
-        //bottom
-        if(i+1 < grid.length && grid[i+1][j]=='1') DFS(grid, i+1, j);        
-        //right
-        if(j+1 < grid[i].length && grid[i][j+1]=='1') DFS(grid, i, j+1);
-        //left
-        if(j-1 >= 0 && grid[i][j-1]=='1') DFS(grid, i, j-1);
+    public void dfsHelper(char[][] grid, int i, int j){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[i].length || grid[i][j] != '1') return;
+
+        grid[i][j] = '0';
+        dfsHelper(grid, i+1, j);
+        dfsHelper(grid, i-1, j);
+        dfsHelper(grid, i, j+1);
+        dfsHelper(grid, i, j-1);
     }
-}
+}/*
+Time Complexity: O(R*C)
+Space Complexity: O(R*C)
+*/
